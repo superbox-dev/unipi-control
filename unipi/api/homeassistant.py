4fcb7abc672bca294e8c05afe3dcc40438dab05c
Michael Hacker <mh@superbox.one>
2021-09-07 19:18:43 +0200

    Added digital inputs to ha discovery

--- unipi/api/homeassistant.py
+++ unipi/api/homeassistant.py
@@ -19,39 +19,73 @@ class HomeAssistant:
     def _read_firmware(self, value_path) -> str:
         group: str = os.path.abspath(os.path.join(os.path.realpath(value_path), *([os.pardir] * 2)))
         firmeware: str = os.path.join(group, "firmware_version")
-        
+
         with open(firmeware, "r") as f:
             return f.read().rstrip()
+
+    def _publish_relay(self, key, device_class) -> None:
+        topic: str = f"""{HA["discovery_prefix"]}/switch/unipi/{device_class.circuit}/config"""
+        firmware: str = self._read_firmware(device_class.value_path)
+
+        discovery: dict = {
+            "name": device_class.dev_name,
+            "unique_id": device_class.circuit,
+            "state_topic": f"{key}/get",
+            "command_topic": f"{key}/set",
+            "payload_on": "1",
+            "payload_off": "0",
+            "value_template": "{{ value_json.value }}",
+            "qos": 1,
+            "retain": "true",
+            "device": {
+                "name": f"""{HA["device"]["manufacturer"]} {device_class.dev_name}""",
+                "identifiers": device_class.circuit,
+                "sw_version": firmware,
+                **HA["device"],
+            }
+        }
+
+        payload: str = json.dumps(discovery)
+        rc, mid = self.client.publish(topic, payload, qos=1, retain=True)
+
+        self._mqtt_log(rc, mid, topic, payload)
+
+    def _publish_input(self, key, device_class) -> None:
+        topic: str = f"""{HA["discovery_prefix"]}/binary_sensor/unipi/{device_class.circuit}/config"""
+        firmware: str = self._read_firmware(device_class.value_path)
+
+        discovery: dict = {
+            "name": device_class.dev_name,
+            "unique_id": device_class.circuit,
+            "state_topic": f"{key}/get",
+            "payload_on": "1",
+            "payload_off": "0",
+            "value_template": "{{ value_json.value }}",
+            "qos": 1,
+            "device": {
+                "name": f"""{HA["device"]["manufacturer"]} {device_class.dev_name}""",
+                "identifiers": device_class.circuit,
+                "sw_version": firmware,
+                **HA["device"],
+            }
+        }
+
+        payload: str = json.dumps(discovery)
+        rc, mid = self.client.publish(topic, payload, qos=1, retain=True)
+
+        self._mqtt_log(rc, mid, topic, payload)
+
+    def _mqtt_log(self, rc, mid, topic, payload) -> None:
+        if rc == mqtt.MQTT_ERR_SUCCESS:
+            logger.info(f"Send `{payload}` to topic `{topic}` - Message ID: {mid}")
+        else:
+            logger.error(f"Failed to send message to topic `{topic}` - Message ID: {mid}")
 
     def publish(self) -> None:
         """Publish topics for discovery."""
         for key, device_class in self.devices.items():
             if device_class.dev == "relay":
-                topic: str = f"""{HA["discovery_prefix"]}/switch/unipi/{device_class.circuit}/config"""
+                self._publish_relay(key, device_class)
+            elif device_class.dev == "input":
+                self._publish_input(key, device_class)
 
-                firmware: str = self._read_firmware(device_class.value_path)
-
-                discovery: dict = {
-                    "name": device_class.dev_name,
-                    "unique_id": device_class.circuit,
-                    "state_topic": f"{key}/get",
-                    "command_topic": f"{key}/set",
-                    "payload_on": "1",
-                    "payload_off": "0",
-                    "value_template": "{{ value_json.value }}",
-                    "qos": 1,
-                    "retain": "true",
-                    "device": {
-                        "name": f"""{HA["device"]["manufacturer"]} {device_class.dev_name}""",
-                        "identifiers": device_class.circuit,
-                        "sw_version": firmware,
-                        **HA["device"],
-                    }
-                }
-
-                payload: str = json.dumps(discovery)
-                rc, mid = self.client.publish(topic, payload, qos=1, retain=True)    
-                if rc == mqtt.MQTT_ERR_SUCCESS:
-                    logger.info(f"Send `{payload}` to topic `{topic}` - Message ID: {mid}")
-                else:
-                    logger.error(f"Failed to send message to topic `{topic}` - Message ID: {mid}")
