178eea84c94caeddab476bd1860ee16a4d59a053
Michael Hacker <mh@superbox.one>
2021-11-28 03:37:30 +0100

    Fixed package structure

--- src/unipi_control/__init__.py
+++ src/unipi_control/__init__.py
@@ -4,4 +4,4 @@ import sys
 # https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
