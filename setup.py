fe0e36e678a1e19fca7356cc0c7c923255e10fbb
Michael Hacker <mh@superbox.one>
2022-01-29 14:59:25 +0100

    Updated package infos

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+import os
+import sys
+
 from setuptools import setup
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 
-setup(install_requires=requirements)
+setup()
