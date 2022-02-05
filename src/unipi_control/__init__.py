70241f53dc776ca31b52ea2b0f88ae62508ae936
Michael Hacker <mh@superbox.one>
2022-02-05 05:40:16 +0100

    Added verbose to workflow

--- src/unipi_control/__init__.py
+++ src/unipi_control/__init__.py
@@ -4,4 +4,4 @@ import sys
 # https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-__version__ = "1.0.3-beta"
+__version__ = "1.0.3-beta1"
