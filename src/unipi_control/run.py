a02cca7d279f849f422c3b32d23cda98a7e00ed9
Michael Hacker <mh@superbox.one>
2022-02-05 08:26:37 +0100

    Fixed systemctl output

--- src/unipi_control/run.py
+++ src/unipi_control/run.py
@@ -152,7 +152,7 @@ def install_unipi_control():
 
     if enable_and_start_systemd.lower() == "y":
         print(colored('-> Enable systemd service "unipi-control.service"', "green"))
-        status: str = str(subprocess.check_output("systemctl enable --now unipi-control", shell=True))
+        status = subprocess.check_output("systemctl enable --now unipi-control", shell=True)
 
         if status:
             logger.info(status)
