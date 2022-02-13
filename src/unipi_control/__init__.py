a1514efa995c0e308207ca147ff7d7f490bd1a52
Michael Hacker <mh@superbox.one>
2022-02-13 19:44:12 +0100

    Added noqa

--- src/unipi_control/__init__.py
+++ src/unipi_control/__init__.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from .version import __version__
+from .version import __version__  # noqa
 
 # https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
