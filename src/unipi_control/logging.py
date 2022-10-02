91b09d2818de89a5c1b757a5c46c8fc16d2fa782
Michael Hacker <mh@superbox.one>
2022-10-02 05:49:26 +0200

    Removed unused code

--- src/unipi_control/logging.py
+++ src/unipi_control/logging.py
@@ -5,7 +5,6 @@ from typing import Dict
 from typing import Final
 
 LOG_NAME: Final[str] = "unipi-control"
-LOG_FMT: Final[str] = "{level} | {message}"
 LOG_MQTT_PUBLISH: Final[str] = "[MQTT] [%s] Publishing message: %s"
 LOG_MQTT_SUBSCRIBE: Final[str] = "[MQTT] [%s] Subscribe message: %s"
 LOG_MQTT_SUBSCRIBE_TOPIC: Final[str] = "[MQTT] Subscribe topic %s"
