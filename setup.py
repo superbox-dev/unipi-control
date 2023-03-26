c6c9e1767691abbc45cc11389ecdfb4e75677284
Michael Hacker <mh@superbox.one>
2023-03-26 06:55:18 +0200

    Added requirements.txt

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
+from pathlib import Path
+from typing import Optional
+
 from setuptools import setup
 
-setup()
+dependencies: dict = {}
+
+with Path("requirements.txt").open(encoding="utf-8") as reqs:
+    group: Optional[str] = None  # pylint: disable=invalid-name
+
+    for line in reqs.read().split("\n"):
+        if not line:
+            group = None
+        elif line.startswith("# install:"):
+            group = line.split(":")[1]
+            dependencies[group] = []
+        elif not line.startswith("#") and group:
+            dependencies[group].append(line)
+
+install_req = dependencies["required"]
+del dependencies["required"]
+
+setup(
+    install_requires=install_req,
+    extras_require=dependencies,
+    package_data={
+        "unipi-control": ["py.typed"],
+    },
+)
