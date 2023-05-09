9d9ad016bb425654a7e9719232a6201f4dadf525
Michael Hacker <mh@superbox.one>
2023-05-09 05:40:19 +0200

    Read version from file

--- src/unipi_control/__init__.py
+++ src/unipi_control/__init__.py
@@ -1,1 +1,5 @@
-from .version import __version__  # noqa: F401
+import os
+from pathlib import Path
+
+version: str = Path("version.txt").read_text(encoding="utf-8").replace("\n", "")
+__version__ = os.environ.get("PACKAGE_VERSION", version)
--- src/unipi_control/version.py
+++
@@ -1,3 +0,0 @@
-import os
-
-__version__ = os.environ.get("PACKAGE_VERSION", "2023.8")
